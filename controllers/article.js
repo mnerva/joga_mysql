aff6439091f6fa43652ee8d4d7f5285ecfd59902
Mona <mona-rea.nerva@voco.ee>
2023-11-01 15:43:02 +0200

    Created author controller and route, refactored application code

---
+++ controllers/article.js
@@ -0,0 +1,40 @@
+// import database connection
+const con = require('../utils/db');
+
+// show all articles - index page
+const getAllArticles = (req, res) => {
+	let query = "SELECT * FROM article";
+	let articles = []
+	con.query(query, (err, result) => {
+		if (err) throw err;
+		articles = result
+		res.render('index', {
+			articles: articles
+		})
+	})
+};
+
+// show article by this slug
+const getArticleBySlug = (req, res) => {
+	let query = `select *, 
+					article.name as article_name, 
+					author.name as author_name 
+					FROM article 
+					INNER JOIN author 
+					ON article.id = article.author_id WHERE slug="${req.params.slug}"`
+	let article
+	con.query(query, (err, result) => {
+		if (err) throw err;
+		article = result
+		console.log(article)
+		res.render('article', {
+			article: article
+		})
+	});
+};
+
+//export controller functions
+module.exports = {
+	getAllArticles,
+	getArticleBySlug
+};
